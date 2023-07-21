6617c663a6eddb24c0e4f51c55655bcadf0a4e9a
Flopiras <Flopiras@yahoo.it>
2023-07-21 15:36:10 +0200

    add actor in movie

--- models/Movie.php
+++ models/Movie.php
@@ -1,15 +1,20 @@
 <?php
+
+include __DIR__ . '/Actor.php';
+
 class Movie
 {
     public $title;
     public $genre;
     public $director;
+    public $mainActor;
 
-    public function __construct($_title, $_genre, $_director)
+    public function __construct($_title, $_genre, $_director, Actor $actor)
     {
         $this->title = $_title;
         $this->genre = $_genre;
         $this->director = $_director;
+        $this->mainActor = $actor->getFullName();
     }
 
     public function IntroduceDirector()
