5541737b8970445c1fde5eec19be86270235e5e1
Flopiras <Flopiras@yahoo.it>
2023-07-21 15:40:32 +0200

    add actors

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
 <?php
 include __DIR__ . '/models/Movie.php';
 
+// istanza attore film 1
+$actor_1 = new Actor('Edward', 'Speleers');
+
+// istanza attore film 2
+$actor_2 = new Actor('Sam', 'Worth');
+
 // istanza film 1
-$movie_1 = new Movie('Eragon', 'fantasy', 'Stefan Fangmeier');
+$movie_1 = new Movie('Eragon', 'fantasy', 'Stefan Fangmeier', $actor_1);
 var_dump($movie_1);
 var_dump($movie_1->IntroduceDirector());
 
 // istanza film 2
-$movie_2 = new Movie('Avatar 2', 'Action', 'James Cameron');
+$movie_2 = new Movie('Avatar 2', 'Action', 'James Cameron', $actor_2);
 var_dump($movie_2);
 var_dump($movie_2->IntroduceDirector());
