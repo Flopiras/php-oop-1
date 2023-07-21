5c2421d9610b1df1da0c609b3c56b82e91fd59b4
Flopiras <Flopiras@yahoo.it>
2023-07-21 15:17:44 +0200

    call function

--- index.php
+++ index.php
@@ -21,7 +21,9 @@ class Movie
 // istanza film 1
 $movie_1 = new Movie('Eragon', 'fantasy', 'Stefan Fangmeier');
 var_dump($movie_1);
+var_dump($movie_1->IntroduceDirector());
 
 // istanza film 2
 $movie_2 = new Movie('Avatar 2', 'Action', 'James Cameron');
 var_dump($movie_2);
+var_dump($movie_2->IntroduceDirector());
