c1045ed38d6997a5759ffe7a4935f73bbf677596
Flopiras <Flopiras@yahoo.it>
2023-07-21 15:17:03 +0200

    instance $movie_2

--- index.php
+++ index.php
@@ -18,6 +18,10 @@ class Movie
     }
 };
 
-
+// istanza film 1
 $movie_1 = new Movie('Eragon', 'fantasy', 'Stefan Fangmeier');
 var_dump($movie_1);
+
+// istanza film 2
+$movie_2 = new Movie('Avatar 2', 'Action', 'James Cameron');
+var_dump($movie_2);
