cfaedca1ac4c6e6bc6d053bca90d822f791b4072
Flopiras <Flopiras@yahoo.it>
2023-07-21 15:02:13 +0200

    add constructor

--- index.php
+++ index.php
@@ -4,4 +4,11 @@ class Movie
     public $title;
     public $genre;
     public $director;
+
+    public function __construct($_title, $_genre, $_director)
+    {
+        $this->title = $_title;
+        $this->genre = $_genre;
+        $this->director = $_director;
+    }
 }
